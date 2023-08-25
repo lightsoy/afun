b55034dc4a6f158cccb3880e96b04e226bca970f
soyeon <y_3305@naver.com>
2023-08-26 02:33:46 +0900

    0825_수정완료

--- js/works.js
+++ js/works.js
@@ -2,18 +2,18 @@
 // console.log($site.eq());
 
 // 안된다...
-siteLinkEvent();
-function siteLinkEvent() {
-    const $site = $(".siteMap .siteLinkWrap .site");
-
-    $site.on("mouseenter", function () {
-        // $(this).closest(".snsIcon").addClass("on");
-        // $(this).closest(".snsIcon").css("opacity: 1");
-        $(this).eq().find(".snsIcon").addClass("on");
-
-    },function () {
-
-        $(this).eq().find(".snsIcon").removeClass("on");
-    });
-
-}
+// siteLinkEvent();
+// function siteLinkEvent() {
+//     const $site = $(".siteMap .siteLinkWrap .site");
+//
+//     $site.on("mouseenter", function () {
+//         // $(this).closest(".snsIcon").addClass("on");
+//         // $(this).closest(".snsIcon").css("opacity: 1");
+//         $(this).eq().find(".snsIcon").addClass("on");
+//
+//     },function () {
+//
+//         $(this).eq().find(".snsIcon").removeClass("on");
+//     });
+//
+// }
