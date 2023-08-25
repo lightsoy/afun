9f14eba7646a9269a4cc189e099011341eabb4fa
soyeon <y_3305@naver.com>
2023-08-25 18:24:30 +0900

    0825_서브페이지 works 반응형, 이미지 소스 수정

--- js/business.js
+++ js/business.js
@@ -5,8 +5,11 @@ $aSlider.slick({
     centerPadding:24,
     slidesToShow: 4,
     focusOnSelect: true,
+    pauseOnHover: true,
+    autoplay:true,
+    autoplaySpeed: 1000,
+    speed: 800,
     arrows: true,
-
     prevArrow: $(".awards .slideArea .prevArrow"),
     nextArrow: $(".awards .slideArea .nextArrow"),
 });
